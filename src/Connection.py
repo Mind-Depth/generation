71c4cddc054c6e0965f69b0c5be5339d1b333a06
Julien Chicha <julien.chicha@epitech.eu>
2019-11-08 14:15:07 +0100

    named-pipe fixes

--- src/Connection.py
+++ src/Connection.py
@@ -13,75 +13,82 @@ from attrdict import AttrDict
 from Configuration import Configuration
 
 class Connection:
-    '''Double read & write pipes wrapper'''
+	'''Double read & write pipes wrapper'''
 
-    CONNECTION_TIMEOUT = 100
+	CONNECTION_TIMEOUT = 100
 
-    def __init__(self, name):
-        '''Creates pipes objects'''
-        self.name = name
-        self.running = False
-        self.log = logging.getLogger(f'Connection({self.name})')
-        self.pipe_in = win32pipe.CreateNamedPipe(
-            r'\\.\pipe\{}_{}'.format(self.name, Configuration.connection.client_to_server),
-            win32pipe.PIPE_ACCESS_INBOUND |  win32file.FILE_FLAG_OVERLAPPED,
-            win32pipe.PIPE_TYPE_MESSAGE | win32pipe.PIPE_READMODE_MESSAGE | win32pipe.PIPE_WAIT,
-            1, Configuration.connection.chunk_size, Configuration.connection.chunk_size, 0, None)
-        self.pipe_out = win32pipe.CreateNamedPipe(
-            r'\\.\pipe\{}_{}'.format(self.name, Configuration.connection.server_to_client),
-            win32pipe.PIPE_ACCESS_OUTBOUND | win32file.FILE_FLAG_OVERLAPPED,
-            win32pipe.PIPE_TYPE_MESSAGE | win32pipe.PIPE_READMODE_MESSAGE | win32pipe.PIPE_WAIT,
-            1, Configuration.connection.chunk_size, Configuration.connection.chunk_size, 0, None)
-        self.pipes = [self.pipe_in, self.pipe_out]
-        self.log.info('Created')
+	def __init__(self, name):
+		'''Creates pipes objects'''
+		self.name = name
+		self.running = False
+		self.log = logging.getLogger(f'Connection({self.name})')
+		self.pipe_in = win32pipe.CreateNamedPipe(
+			r'\\.\pipe\{}_{}'.format(self.name, Configuration.connection.client_to_server),
+			win32pipe.PIPE_ACCESS_INBOUND |  win32file.FILE_FLAG_OVERLAPPED,
+			win32pipe.PIPE_TYPE_MESSAGE | win32pipe.PIPE_READMODE_MESSAGE | win32pipe.PIPE_WAIT,
+		1, Configuration.connection.chunk_size, Configuration.connection.chunk_size, 0, None)
+		self.pipe_out = win32pipe.CreateNamedPipe(
+			r'\\.\pipe\{}_{}'.format(self.name, Configuration.connection.server_to_client),
+			win32pipe.PIPE_ACCESS_OUTBOUND | win32file.FILE_FLAG_OVERLAPPED,
+			win32pipe.PIPE_TYPE_MESSAGE | win32pipe.PIPE_READMODE_MESSAGE | win32pipe.PIPE_WAIT,
+		1, Configuration.connection.chunk_size, Configuration.connection.chunk_size, 0, None)
+		self.pipes = [self.pipe_in, self.pipe_out]
+		self.log.info('Created')
 
-    def _hide_legitimate_errors(f):
-        def func(self, *args, **kwargs):
-            try:
-                return f(self, *args, **kwargs)
-            except pywintypes.error as e:
-                if self.running:
-                    raise
-        func.__name__ = f.__name__
-        return func
+	def _hide_legitimate_errors(f):
+		def func(self, *args, **kwargs):
+			try:
+				return f(self, *args, **kwargs)
+			except pywintypes.error as e:
+				if self.running:
+					raise
+		func.__name__ = f.__name__
+		return func
 
-    @_hide_legitimate_errors
-    def read(self):
-        '''Reads a json object'''
-        error, msg = win32file.ReadFile(self.pipe_in, Configuration.connection.chunk_size)
-        assert not error, error
-        s = msg.rstrip(b'\x00').decode()
-        self.log.info(f'Recv:{s}')
-        return None if not s else AttrDict(json.loads(s))
+	@_hide_legitimate_errors
+	def read(self):
+		'''Reads a json object'''
+		error, msg = win32file.ReadFile(self.pipe_in, Configuration.connection.chunk_size)
+		assert not error, error
+		s = msg.rstrip(b'\x00').decode()
+		self.log.info(f'Recv:{s}')
+		return None if not s else AttrDict(json.loads(s))
 
-    @_hide_legitimate_errors
-    def write(self, msg):
-        '''Writes a json object'''
-        s = json.dumps(msg)
-        win32file.WriteFile(self.pipe_out, (s + '\r\n').encode())
-        self.log.info(f'Send:{s}')
+	@_hide_legitimate_errors
+	def write(self, msg):
+		'''Writes a json object'''
+		s = json.dumps(msg)
+		win32file.WriteFile(self.pipe_out, (s + '\r\n').encode())
+		self.log.info(f'Send:{s}')
 
-    def start(self):
-        '''Connects pipes'''
-        self.log.info('Blocked')
-        self.running = True
-        overlapped = pywintypes.OVERLAPPED()
-        for pipe in self.pipes:
-            win32pipe.ConnectNamedPipe(pipe, overlapped)
-        while True:
-            code = win32event.WaitForMultipleObjects(self.pipes, True, self.CONNECTION_TIMEOUT)
-            if code != win32event.WAIT_TIMEOUT:
-                break
-        self.log.info('Unblocked')
+	def start(self):
+		'''Connects pipes'''
+		self.log.info('Blocked')
+		self.running = True
+		overlapped = pywintypes.OVERLAPPED()
+		for pipe in self.pipes:
+			win32pipe.ConnectNamedPipe(pipe, overlapped)
+		unconnected = list(self.pipes)
+		while unconnected:
+			code = win32event.WaitForMultipleObjects(unconnected, True, self.CONNECTION_TIMEOUT)
+			if code != win32event.WAIT_TIMEOUT:
+				break
+			for i in range(len(unconnected)):
+				pipe = unconnected.pop(0)
+				try:
+					win32pipe.GetNamedPipeClientProcessId(pipe)
+				except pywintypes.error as e:
+					unconnected.append(pipe)
+		self.log.info('Unblocked')
 
-    def stop(self):
-        '''Closes pipes'''
-        if not self.running:
-            return
-        self.running = False
-        for pipe in self.pipes:
-            win32file.CloseHandle(pipe)
-        self.log.info('Stopped')
+	def stop(self):
+		'''Closes pipes'''
+		if not self.running:
+			return
+		self.running = False
+		for pipe in self.pipes:
+			win32file.CloseHandle(pipe)
+		self.log.info('Stopped')
 
 class ConnectionGroup:
     '''Manages several Connection objects with threads'''
